f2788b93209ed08336d7e9e94ae2f3f6e4250ce3
TGW <mc-casper@hotmail.dk>
2023-12-01 12:13:25 +0100

    Implements health checks and kubernetes integration

---
+++ GameServer/HealthChecks.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace GameServer
+{
+    public class ReadinessHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+
+    public class LivenessHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy());
+        }
+    }
+}
